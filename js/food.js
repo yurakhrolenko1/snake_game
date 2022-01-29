930b7fe718ecc97795cc4eb79486e220cf87a7c5
yurakhrolenko1 <yurakhrolenko1@gmail.com>
2022-01-29 12:48:00 +0200

    added function to expend snake when it's on top of food

--- js/food.js
+++ js/food.js
@@ -1,16 +1,18 @@
 import {
-    onSnake
+    onSnake,
+    expendSnake
 } from './snake.js'
 
-
+const EXPENTION_RATE = 1
 let foodCoords = {x: 5, y: 5}
 
 
 export function updateFood() {
     // if snake on food move food to new coords
     if (onSnake(foodCoords)) {
+        expendSnake(EXPENTION_RATE)
         foodCoords = {x: 15, y: 15}
-        console.log("on top")
+        console.log("on top OF FOOD")
     }
     // then add new segment to snake
 
--- js/snake.js
+++ js/snake.js
@@ -1,5 +1,6 @@
 import { getDirection } from "./input.js"
 
+let newSegments = 0
 export const SNAKE_SPEED = 3  // updates of gameloop per second
 const snakeBody = [
     {"x": 11,"y": 11}
@@ -7,6 +8,8 @@ const snakeBody = [
 
 
 export function updateSnake() {
+    addSegments()
+
     const inputDirection = getDirection()
     for (let i = snakeBody.length -2 ; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] }
@@ -28,7 +31,25 @@ export function drawSnake(gameBoard) {
 
 export function onSnake(position) {
     return snakeBody.some( (segment) => {
-        return segment.x === position.x && segment.y === position.y
+        return equalPositions(segment, position)
     })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+
+export function expendSnake(amount) {
+    newSegments += amount
+}
+
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+    // console.log(snakeBody)
+    }
+    newSegments = 0
 }
 
